4b7b444d4a8854154bd2dd5ce67f5951cdf67577
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-21 11:22:25 +0200

    update package.json

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const colors = require('colors');
 const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
