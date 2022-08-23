31cd4d926fbc9134ec20c50e5a938c3e92b5be49
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-23 20:55:44 +0200

    update uri

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: 'https://project-mgmt-app-gql.herokuapp.com/',
+  uri: 'ttp://localhost:5000/graphql',
   cache,
 });
 
