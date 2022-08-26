dd9a245d267c42f43737aa14b309195accf7ae63
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-26 20:47:22 +0200

    Client routing

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ connectDB();
 
 // serve react content
 app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.use(cors());
 
